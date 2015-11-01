1b371ded95cf2c03c2a307fb1428f281de9c0412
Vilyan01 <bheller2010@live.com>
2015-11-01 15:24:47 -0800

    Initial Commit

---
+++ ActivityThrobberDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ActivityThrobberDemo
+//
+//  Created by Brian Heller on 11/1/15.
+//  Copyright © 2015 Reaper Sofware Solution. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
